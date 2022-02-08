8f69c18d809e9c0a35eebd8c84c36ebc043575a2
frissons <dodongbren@gmail.com>
2022-02-09 01:08:41 +0800

    buang ang git

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const colors = require('colors')
 require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware')
-
+const connectDB = require('./config/db')
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+connectDB()
 app.use(express.json());
 app.use(errorHandler);
 app.use(express.urlencoded({extended: false}))

8b0cc21a2c2a54349987a08a2812d50697e88074
frissons <dodongbren@gmail.com>
2022-02-08 19:06:31 +0800

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const app = express();
+const port = 5000;
+
+
+
+app.listen(port, () => console.log(`Server started on ${port}`))
+ 
